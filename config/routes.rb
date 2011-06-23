32ac4064a080a2ea9c8e8590e02f3cdd821122c0
Yerong Chen <jarricochen@gmail.com>
2011-06-22 22:26:38 -0400

    Finished first cut of the User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  
+  resources :users
+  
+  #get "users/new"
+  
+  #get "user/show"
 
   get "pages/home"
-
   get "pages/contact"
-
   get "pages/about"
-
   get "pages/help"
   
+  
   match '/signup',    :to => 'users#new'
-
   match '/contact',   :to => 'pages#contact'
   match '/about',     :to => 'pages#about'
   match '/help',      :to => 'pages#help'
